1f94c2c4fae05fa2d4fab84da1e7b16fe77d3ebb
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-04-24 10:00:15 +0300

    Create Main.java

---
+++ Задача_1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+         public static void main(String[] args) {
+             Integer a=2;
+             Integer b=8;
+             System.out.print(a.toString()+b.toString());
+         }
+}
