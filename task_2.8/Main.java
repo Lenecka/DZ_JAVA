93eb196ea14f47390cb8baf7bdf8a77b2acd8070
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-04-27 19:22:38 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       /*Задание 2,8: С помощью цикла for сформируйте строку '123456789' и запишите ее в переменную str.
+        */
+        String str="";
+        for(int i=1;i<10;i++)
+        str=str+i;
+        System.out.print(str);
+        }
+
+    }
