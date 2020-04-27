c28d5e5be5bf9db1a16b1fc2922f16a7cf16293a
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-04-27 15:02:44 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       /*Задание 2,7: Выведите в консоль столбец чисел от 1 до 50.
+        */
+        for(int i=1;i<51;i++)
+            System.out.println(i);
+        }
+
+    }
