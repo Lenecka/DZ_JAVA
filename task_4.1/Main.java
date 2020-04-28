3ad8e6996c4eede973e878c47c5e054c5647aefa
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-04-28 11:08:55 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -3,17 +3,20 @@ public class Main {
     public static void main(String[] args) {
         /*Задача 4.1 Написать функцию, которая возвращает минимум из двух чисел.
          */
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите число 1   ");
         int a = scan.nextInt();
         System.out.print("Введите число 2   ");
         int b = scan.nextInt();
-        min(a, b);
-    }
+       // min(5,7);
+        System.out.print("Минимальное чосло равно: ");
+        System.out.print(min(a,b));
 
-    static void min(int a, int b) {
-        if (a > b) System.out.println("Минимальное число "+b);
-        else if(a<b)System.out.println("Минимальное число "+a);
-        else System.out.println("Числа равны");
+    }
+    static int min(int a, int b) {
+        if (a > b) return b;
+        else if(a<b)return a;
+        else return a=b;
     }
 }
