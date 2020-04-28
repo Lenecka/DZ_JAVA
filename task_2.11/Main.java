350d1bc523aa846496b32787053af7af3fa36008
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-04-28 10:02:58 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -4,29 +4,28 @@ public class Main {
        /*Задание 2,11: В переменной min лежит число от 0 до 59.
         Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
         */
-        int[] mas =new int[60];
+       /* int[] mas =new int[60];
         for (int i=0;i<=59;i++) {
-             mas[i] = i;
-         }
+            mas[i] = i;
+        }
         System.out.println(Arrays.toString(mas));
         int min=new Random().nextInt(mas.length);
         System.out.println("Ваше число из множества: "+min);
-       for(int i=0;i<mas.length/4;i++) {
-           if (min == mas[i])
+          */
+         Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число от 0 до 59   ");
+        double cislo=scan.nextInt();
+        if(cislo>=0&&cislo<=59) {
+            if (cislo>=0&&cislo<=(60/4))
                System.out.println("Ваше число попало в первую четверть часа");
-       }
-        for(int i=mas.length/4+1;i<mas.length/4*2;i++) {
-            if (min == mas[i])
+               else if (cislo>=(60/4+1)&&cislo<=(60/4*2))
                 System.out.println("Ваше число попало во вторую четверть часа");
+                    else if (cislo>=(60/4*2+1)&&cislo<=(60/4*3))
+                    System.out.println("Ваше число попало в тетью четверть часа");
+                        else if (cislo>=(60/4*3+1)&&cislo<=60)
+                        System.out.println("Ваше число попало в четвертую четверть часа");
         }
-        for(int i=mas.length/4*2+1;i<mas.length/4*3;i++) {
-            if (min == mas[i])
-                System.out.println("Ваше число попало в тетью четверть часа");
-        }
-        for(int i=mas.length/4*3+1;i<mas.length;i++) {
-            if (min == mas[i])
-                System.out.println("Ваше число попало в четвертую четверть часа");
-        }
+        else System.out.println("Вы ввели неверное число. Попробуйте снова");
     }
 
 }
