2cb3d4530395a9207dbaf7eec503ac97103b91b5
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-04-29 23:18:24 +0300

    Create Main.java

---
+++ task_5.1/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        /*Задача 5.1
+        /* /* Помогите коту обрести имя с помощью метода setName. */
+
+                Cat cat = new Cat();
+                cat.setName("Жужик");
+                System.out.println(cat.name);
+            }
+        }
+        class Cat {
+            String name = "безымянный кот";
+            public void setName(String name) {
+                this.name=name;
+                }
+
+        }
