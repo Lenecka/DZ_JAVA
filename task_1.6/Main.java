ddb01e6a16bcf37b6207830a60b5622920d096a3
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-06 12:16:28 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+         public static void main(String[] args) {
+            int number=9;
+            int stepen=2;
+            System.out.print("Cislo 9 v kvadrate ravno  " + Math.pow(number,stepen));
+         }
+}
+
+/*
+public class Main {
+         public static void main(String[] args) {
+            int number=9;
+            for(int i=1; i<2;i++)number*=9;
+             {
+                 System.out.print(number);
+             }
+
+         }
+}
+*/
