113902d5583f689adaa40177b1bf4af9fa1ec186
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-06 11:35:43 +0300

    Create Main.java

---
+++ task_1.2/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+         public static void main(String[] args) {
+            double x=7.5;
+            double a=x*2;
+            double b=x*3;
+            System.out.print("a="+a+" "+"b="+b);
+    }
+}
