efb5429b4fb48c971b01055ccecc806fc35e962d
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-06 12:15:28 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+         public static void main(String[] args) {
+            String name="Alena";
+            int age=27;
+            String city="Moskva";
+            System.out.print("name:"+name+"   age:"+age+" let"+"   city:"+city);
+
+         }
+}
+
