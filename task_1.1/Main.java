3149443f7314ec1c6b2728064556f4b855b55097
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-06 11:35:02 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+         public static void main(String[] args) {
+            int numbe1=5;
+            int numbe2=9;
+            int numbe3=numbe1+numbe2;
+           System.out.print(numbe3);
+
+         }
+}
