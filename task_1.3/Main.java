ec311c98eb33a49c6e3c1b6f7209ce78cd0278eb
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-06 12:14:22 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+         public static void main(String[] args) {
+             Integer a=2;
+             Integer b=8;
+             System.out.print(a.toString()+b.toString());
+         }
+}
+
+
+
+// public class Main {
+      //   public static void main(String[] args) {
+      //      int a=2;
+      //      int b=8;
+       //     int c=a*10+b;
+       //    System.out.print(c);
+
+      //   }
+// }
