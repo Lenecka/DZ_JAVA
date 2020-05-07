62041f3b2cc0c5aed88f475764280601d4a0e325
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-07 22:06:05 +0300

    Create Main.java

---
+++ task_6.8/Main.java
@@ -0,0 +1,17 @@
+/* Задача 6.8
+Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+int[] m = new int[2];
+m[8] = 5; */
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int[] m = new int[2];
+            m[8] = 5;
+            //System.out.println(m[8]);
+            }catch (Exception e){
+            System.out.println("Ошибка! Выход за пределы массива");
+            e.printStackTrace();
+        }
+    }
+}
