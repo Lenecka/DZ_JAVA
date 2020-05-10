5f679eda534a96303148a946e5572af26824796a
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-10 19:33:49 +0300

    Create Main.java

---
+++ task_7.2/Main.java
@@ -0,0 +1,37 @@
+/*Задача 7.2
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
+Используйте коллекции.
+(Придётся погуглить коллекции)
+*/
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/*
+Номер месяца
+*/
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите имя месяца     ");
+        String date = scan.nextLine();
+        List<String> stringList = new ArrayList<>();//создание нового списка
+        stringList.add("Январь"); //добавление элементов
+        stringList.add("Февраль");
+        stringList.add("Март");
+        stringList.add("Апрель");
+        stringList.add("May");
+        stringList.add("Июнь");
+        stringList.add("Июль");
+        stringList.add("Август");
+        stringList.add("Сентябрь");
+        stringList.add("Октябрь");
+        stringList.add("Ноябрь");
+        stringList.add("Декабрь");
+        int n = stringList.indexOf(date);
+        System.out.println(stringList.size());//размер списка
+        System.out.println(date+" is the "+(n+1)+" month");
+        //stringList.get(0);//получения элемента по индексу (начинается с 0 элемента)
+    }
+}
