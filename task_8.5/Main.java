a5621ba32bfccd725eaad7721da08bdccbfca4db
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-16 20:05:39 +0300

    Update Main.java

--- task_8.5/Main.java
+++ task_8.5/Main.java
@@ -15,55 +15,23 @@
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*//создание папки
-        FileWriter file1 = new FileWriter("/Users/temp/Desktop/test" "Hello"); // напишите тут ваш код
-       // boolean created = file1.mkdir();
-        //if(created)
-         //   System.out.println("Folder has been created");*/
-        //создаем документ txt
-
-        // получаем разделитель пути в текущей операционной системе
-        String fileSeparator = System.getProperty("file.separator");
-
-        //создаем абсолютный путь к файлу
-        String absoluteFilePath = fileSeparator + "Users" + fileSeparator + "temp" + fileSeparator + "Desktop" + fileSeparator + "test" + fileSeparator + "Hello.txt";
-
-        File file = new File(absoluteFilePath);
-        if (file.createNewFile()) {
-            System.out.println(absoluteFilePath + " Файл создан");
-        } else {
-            System.out.println("Файл " + absoluteFilePath + " уже существует");
-        }
-//Оборачивая System.in (стандартный поток ввода) в InputStreamReader, который обернут в BufferedReader , мы можем прочитать ввод от пользователя в командной строке
-// метод readLine () считывает строку текста из командной строки.
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите имя файла: ");
-
-        String name=reader.readLine();
-        String pathToFile="/Users/temp/Desktop/test/" + name;
-        System.out.println("Имя файла: " + name);
-//имя файла прочитали и вывели на экран
         System.out.println("Введите текст. Для выхода введите:  exit");
-        //BufferedReader buff=new BufferedReader(new InputStreamReader(System.in));
-        String stroka="";
-        BufferedWriter bWriter = new BufferedWriter(new FileWriter(pathToFile)); //запись в файл
-        do {
-            String line = reader.readLine();
-            //stroka = line;
-            bWriter.write(line);
-            line += "\n";
-            //bWriter.write(stroka);
+        // stroka="";
+        String line;
+        BufferedWriter bWriter = new BufferedWriter(new FileWriter(reader.readLine())); //запись в файл
+        while (true)
+        {
+            line=reader.readLine();
+            //bWriter.write(line+"\n");
+            if(line.equals("exit")){
+                bWriter.write(line+"\n");
+                break;
+            }
+            else{
+                bWriter.write(line+"\n");
+            }
         }
-        while (!reader.readLine().equals("exit")) ;
-
-            //bWriter.write(stroka);
-
-            //BufferedWriter bWriter = new BufferedWriter(new FileWriter(pathToFile));
-           // String line=reader.readLine();
-       // stroka+=line;
-       // stroka+="\n";
-            //
-
         bWriter.close();
-        }
     }
+}
