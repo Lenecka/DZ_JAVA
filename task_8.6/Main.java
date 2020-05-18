f7fb3bc2ce4d16a870e061b43572bf0f98e672cd
Lenecka <64208802+Lenecka@users.noreply.github.com>
2020-05-18 22:22:42 +0300

    Update Main.java

--- task_8.6/Main.java
+++ task_8.6/Main.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class Main {
     public static void main(String[] args) throws IOException {
-        String fileSeparator = System.getProperty("file.separator");
+      /*  String fileSeparator = System.getProperty("file.separator");
         String absoluteFilePath = fileSeparator + "Users" + fileSeparator + "temp" + fileSeparator + "Desktop" + fileSeparator + "test" + fileSeparator + "Hello.txt";
         File file = new File(absoluteFilePath);
         if (file.createNewFile()) {
@@ -31,29 +31,33 @@ public class Main {
         } else {
             System.out.println("Файл " + absoluteFilePath + " уже существует");
         }
+
+       */
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите имя файла: ");
         String name=reader.readLine();
-        String pathToFile="/Users/temp/Desktop/test/" + name;
+        String pathToFile=name;
         //String name=reader.readLine();
-        System.out.println("Введите числа: ");
-        BufferedReader reader2 = new BufferedReader(new InputStreamReader(new FileInputStream(pathToFile)));
-        ArrayList<Integer> nums=new ArrayList<Integer>(8);
+        //System.out.println("Введите числа: ");
+        reader = new BufferedReader(new FileReader(pathToFile));
+        ArrayList<Integer> nums=new ArrayList<Integer>();
         ArrayList<Integer> nums2=new ArrayList<Integer>();
-       // ArrayList<Integer> nums3=new ArrayList<Integer>();
+        // ArrayList<Integer> nums3=new ArrayList<Integer>();
         int num;
-        for(int i=0;i<8;i++){
+        String c;
+       // while((c=reader.readLine())!=null) {
+        while(reader.ready()) {
             num = Integer.parseInt(reader.readLine().trim());
             nums.add(num);
             if(num%2==0)
                 nums2.add(num);
-       // while((line=reader2.readLine())!=null){
+            // while((line=reader2.readLine())!=null){
             //nums.add(Integer.parseInt(i));
         }
         System.out.println(nums);
         System.out.println(nums2);
-            Collections.sort(nums2);
-               // nums3.add(num);
+        Collections.sort(nums2);
+        // nums3.add(num);
         for (int i : nums2) {
             System.out.println(i);
         }
